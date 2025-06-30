0ea871abe274ff5cdaf1a14febf6d7dbdd3f704b
Hqnnqh <hannah@diefluchs.at>
2025-06-30 17:26:49 +0200

    feat: utilize disko

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, disko, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.chicken = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -14,6 +19,8 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+        disko.nixosModules.disko
+        ./disko/disko-config.nix
       ];
     };
   };
