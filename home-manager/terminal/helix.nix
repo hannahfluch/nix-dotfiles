691c59d933b88cced2f6ee73623efcad6dbc6dc2
Hqnnqh <hannah@diefluchs.at>
2025-07-09 16:15:15 +0200

    chore: restructure terminal programs

--- home-manager/terminal/helix.nix
+++ home-manager/terminal/helix.nix
@@ -1,11 +1,13 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.helix = {
     enable = true;
     defaultEditor = true;
     extraPackages = with pkgs; [
-      (import ./rust-toolchain.nix pkgs)
+      (import ../rust-toolchain.nix pkgs)
       pyright
       ruff
 
@@ -30,53 +32,50 @@
     themes = {
       everblush_transparent = {
         "inherits" = "everblush";
-        "ui.background" = { };
+        "ui.background" = {};
       };
     };
 
     languages = {
       langauge = [
-
         {
           name = "rust";
-          language-servers = [ "rust-analyzer" ];
+          language-servers = ["rust-analyzer"];
           formatter.command = "rustfmt";
           auto-format = true;
         }
 
         {
           name = "python";
-          language-servers = [ "pyright" "ruff" ];
+          language-servers = ["pyright" "ruff"];
         }
         {
           name = "nix";
-          language-servers = [ "nil" ];
+          language-servers = ["nil"];
           formatter.command = "nixfmt";
           auto-format = true;
         }
         {
           name = "c-sharp";
-          language-servers = [ "omnisharp-roslyn" ];
+          language-servers = ["omnisharp-roslyn"];
           formatter.command = "csharpier";
           auto-format = true;
         }
         {
           name = "typescript";
-          language-servers = [ "typescript-language-server" ];
+          language-servers = ["typescript-language-server"];
           formatter.command = "prettier";
           auto-format = true;
         }
         {
           name = "toml";
-          language-servers = [ "taplo" ];
+          language-servers = ["taplo"];
         }
         {
           name = "c";
-          language-servers = [ "clangd" ];
+          language-servers = ["clangd"];
         }
       ];
-
     };
   };
 }
-
