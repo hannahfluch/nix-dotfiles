28abec653a7392943938f6d977e0fb51f800c41a
hannahfluch <hannah@diefluchs.at>
2025-07-17 14:51:45 +0200

    fix: impermanence

--- home-manager/toolchains/dotnet.nix
+++ home-manager/toolchains/dotnet.nix
@@ -10,7 +10,7 @@
     ".aspnet/"
   ];
 
-  persist.cache.contents = [
+  persist.caches.contents = [
     ".nuget/packages/"
     ".templateengine/"
   ];
--- persist/base.nix
+++ persist/base.nix
@@ -1,27 +1,31 @@
-{lib, ...}:
+{ lib, ... }:
 rec {
-  persistentOption = with lib; with lib.types; mkOption {
-    description = "submodule example";
-    type = attrsOf (submodule {
-      options = {
-        prefix = mkOption {
-          default = "/persistent/";
-          type = strMatching "^/([[:alnum:]_]+/)?$";
+  persistentOption =
+    with lib;
+    with lib.types;
+    mkOption {
+      description = "submodule example";
+      type = attrsOf (submodule {
+        options = {
+          prefix = mkOption {
+            default = "/persistent/";
+            type = strMatching "^/([[:alnum:]_]+/)?$";
+          };
+          contents = mkOption {
+            default = [ ];
+            type = nonEmptyListOf nonEmptyStr;
+          };
         };
-        contents = mkOption {
-          default = [ ];
-          type = nonEmptyListOf nonEmptyStr;
-        };
-      };
-    });
-  };
-
+      });
+    };
 
   isDirectory = lib.hasSuffix "/";
   isSystem = lib.hasPrefix "/";
   not = f: a: !(f a);
   stripTrailing = p: lib.substring 0 (builtins.stringLength p - 1) p;
-  filter2 = f: g: c: builtins.filter f (builtins.filter g c);
+  filter2 =
+    f: g: c:
+    builtins.filter f (builtins.filter g c);
 
   parseUserDirectories = paths: map stripTrailing (filter2 (not isSystem) isDirectory paths);
   parseUserFiles = paths: filter2 (not isSystem) (not isDirectory) paths;
