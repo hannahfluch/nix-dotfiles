1600ea7c6d53bb979cac048c51a93ee649af0ff5
hannahfluch <hannah@diefluchs.at>
2025-08-18 21:24:08 +0200

    feat(applications): overwrite binaryninja user dir

--- home/applications/binaryninja.nix
+++ home/applications/binaryninja.nix
@@ -1,8 +1,23 @@
 { pkgs, ... }:
+let
+  dirPath = "/persistent/data/home/hannah/.binaryninja";
+in
 {
-  home.packages = [ pkgs.binaryninja-free ];
+  home.packages = [
+    (pkgs.binaryninja-free.overrideAttrs
+
+      (
+        finalAttrs: previousAttrs: {
+          nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+          postInstall = ''
+            wrapProgram $out/bin/binaryninja --set BN_USER_DIRECTORY ${dirPath}
+            ${previousAttrs.postInstall or ""}
+          '';
+        }
+      )
+    )
+  ];
   persist.data.contents = [
     ".config/Vector 35/Binary Ninja.conf"
-    ".binaryninja/"
   ];
 }
