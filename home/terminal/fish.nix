b425f1e51d143236350c721d82ed39961d3e78bd
hannahfluch <hannah@diefluchs.at>
2025-08-26 23:54:05 +0200

    feat: add specialisations for wallpapers

--- home/terminal/fish.nix
+++ home/terminal/fish.nix
@@ -1,9 +1,36 @@
-{ ... }: {
+{ ... }:
+{
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
     '';
+    functions = {
+      switchwall = ''
+        if test (count $argv) -lt 1
+          echo "Usage: switchwall <wallpaper name>"
+          return 1
+        end
+
+        if test "$argv[1]" = "-l"
+          echo "Available specialisations:"
+          for dir in /nix/store/*-home-manager-generation/specialisation/*
+              echo (basename $dir)
+          end | sort -u
+          return 0
+        end
+
+        set wallpaper $argv[1]
+        set activate (ls -d /nix/store/*-home-manager-generation/specialisation/$wallpaper/activate 2>/dev/null | sort | tail -n 1)
+        if test -z "$activate"
+          echo "No specialisation '$wallpaper' found in /nix/store"
+          return 1
+        end
+
+        echo "Activating specialisation: $wallpaper"
+        $activate
+      '';
+    };
   };
 
   persist.data.contents = [
--- home/wm/stylix.nix
+++ home/wm/stylix.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  wallpapers = lib.filesystem.listFilesRecursive ../../assets/wallpapers;
+in
 {
   stylix.enable = true;
-  stylix.image = ../../assets/wallpapers/mountains.jpg;
   stylix.fonts.monospace = {
     name = "MesloLGSNF";
     package = pkgs.meslo-lgs-nf;
@@ -9,4 +11,26 @@
   stylix.polarity = "dark";
   stylix.opacity.terminal = 0.65;
   stylix.overlays.enable = false;
+
+  # make every wallpaper it's own specialisation
+  specialisation = builtins.listToAttrs (
+
+    map (path: {
+      # strip suffix if there is one
+      name =
+        let
+          parts = lib.splitString "." (builtins.baseNameOf path);
+        in
+        if builtins.length parts > 1 then
+          lib.removeSuffix ("." + lib.last parts) (builtins.baseNameOf path)
+        else
+          builtins.baseNameOf path;
+      # set image as stylix wallpaper
+      value = {
+        configuration.stylix.image = lib.mkForce path;
+      };
+    }) wallpapers
+
+  );
+  stylix.image = ../../assets/wallpapers/mountains.jpg; # default wallpaper
 }
