426d7e72a3b62df86f627783338068ce9bb9ffbc
hannahfluch <hannah@diefluchs.at>
2025-08-30 14:15:12 +0200

    feat(toolchains-python): configure uv

--- home/toolchains/python.nix
+++ home/toolchains/python.nix
@@ -1,17 +1,39 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  py = pkgs.python3.withPackages (
+    pythonPackages: with pythonPackages; [
+      virtualenv
+      requests
+      pwntools # todo: shellcraft requires gcc at runtime
+      angr
+      z3-solver
+      libdebug
+    ]
+  );
+in
 {
-  home.packages = with pkgs; [
-    (python3.withPackages (
-      pythonPackages: with pythonPackages; [
-        virtualenv
-        requests
-        pwntools # todo: shellcraft requires gcc at runtime
-        angr
-        z3-solver
-        libdebug
-      ]
-    ))
-    ruff
-    uv
-  ];
+
+  home.packages = [ py ];
+
+  programs.uv = {
+    enable = true;
+    settings = {
+      python-downloads = "never";
+      python-preference = "system";
+      pip.python = py;
+    };
+  };
+
+  programs.ruff = {
+    enable = true;
+    settings = {
+      line-length = 120;
+      cache-dir = config.xdg.cacheHome + "/ruff";
+      lint.ignore = [
+        # * import related
+        "F403"
+        "F405"
+      ];
+    };
+  };
 }
