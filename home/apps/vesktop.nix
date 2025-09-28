21d61f8b63b753b63abe8dfd1fd2f4d250935802
hannahfluch <hannah@diefluchs.at>
2025-09-28 10:48:17 +0200

    feat(apps-vesktop): configure plugins

--- home/apps/vesktop.nix
+++ home/apps/vesktop.nix
@@ -4,17 +4,32 @@
   ...
 }:
 {
-  home.packages = [
-    (pkgs.vesktop.overrideAttrs (
-      finalAttrs: previousAttrs: {
-        postUnpack = ''
-          cp ${assets.outPath}/custom_vesktop.gif $sourceRoot/static/shiggy.gif
+  programs.vesktop = {
+    enable = true;
+    package = (
+      pkgs.vesktop.overrideAttrs (
+        finalAttrs: previousAttrs: {
+          postUnpack = ''
+            cp ${assets.outPath}/custom_vesktop.gif $sourceRoot/static/shiggy.gif
 
-          ${previousAttrs.postUnpack or ""}
+            ${previousAttrs.postUnpack or ""}
 
-        '';
-      }
-    ))
-  ];
+          '';
+        }
+      )
+    );
+    vencord.settings = {
+      autoUpdate = false;
+      autoUpdateNotification = false;
+      useQuickCss = true;
+      notifyAboutUpdates = false;
+      plugins = {
+        FakeNitro.enabled = true;
+        UserMessagesPronouns.enabled = true;
+        CopyFileContents.enabled = true;
+      };
+
+    };
+  };
   persist.data.contents = [ ".config/vesktop/" ];
 }
